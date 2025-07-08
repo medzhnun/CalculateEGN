ca5083c4d70b49e9677bbd37213afbce6c67cb1a
Medzhnun.Mehmed <Medzhnun.Mehmed@inbox.ru>
2025-07-08 10:08:42 +0300

    correcting checks for EGN

--- src/calculateegn/CheckEGN.java
+++ src/calculateegn/CheckEGN.java
@@ -1,38 +1,35 @@
 
 package calculateegn;
 
+
 /**
  *
  * @author Medzhnun Mehmed
  */
 public class CheckEGN {
-    private int year;
-    private int month;
-    private int day;
-    private boolean isFemale;
-    private Province province;
-    private boolean valid = false;
+
     private final int[] weights = {2, 4, 8, 5, 10, 9, 7, 3, 6};
     
-    public CheckEGN(int year, int month, int day, boolean isFemale, Province province) {
-        this.year = year;
-        this.month = month;
-        this.day = day;
-        this.isFemale = isFemale;
-        this.province = province;
-    }
-    
-    public boolean egnValidation(){
-        if (year <1900) {
-            month+=20;
+      
+    public boolean validateEGN(String egn){
+        int tempEGN = 0;
+        int checkDigit;
+        if (egn.isEmpty() || egn.length() != 10 || !egn.matches("[0-9]+")) {
+            throw new IllegalArgumentException("Липсващо или неправилно ЕГН");
+        } else if(Integer.parseInt(egn.substring(6, 9))>999 || Integer.parseInt(egn.substring(6, 9)) < 0){
+            throw new IllegalArgumentException("Не съществуващ регион");
+        } else if ((Integer.parseInt(egn.substring(2, 4)) > 12 && 
+                      Integer.parseInt(egn.substring(2, 4)) != 32 &&
+                      Integer.parseInt(egn.substring(2, 4)) !=52) ||
+                      Integer.parseInt(egn.substring(4, 6)) > 31){
+            throw new IllegalArgumentException("Неправилна дата на раждане");
+        } else {
+            for (int i = 0; i < egn.length()-1; i++) {
+                tempEGN += (egn.charAt(i)-'0')*weights[i];
+            }
+            checkDigit= tempEGN%11;
         }
-        if (year>1999) {
-            month+=40;
-        }
-        year = year%100;
         
-        
-        return true;
+        return Integer.parseInt(egn.substring(egn.length()-1)) == checkDigit;
     }
-    
 }
